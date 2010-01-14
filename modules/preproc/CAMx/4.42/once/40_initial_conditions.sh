8de011288296b78ac8bae06feef4e64979efbfaf
Daniel C. Oderbolz <daniel.oderbolz@psi.ch>
2010-01-14 14:38:05 +0000

    %B

--- modules/preproc/CAMx/4.42/once/40_initial_conditions.sh
+++ modules/preproc/CAMx/4.42/once/40_initial_conditions.sh
@@ -260,6 +260,9 @@ function initial_conditions()
 						# Close the bracket
 						extra="${extra}\}"
 						
+						# Add the rest of the syntax
+						extra=",extra\=${extra}"
+						
 					elif [ "${CXR_IC_BC_TC_METHOD}" == MOZART_INCREMENT ]
 					then
 					
@@ -287,6 +290,9 @@ function initial_conditions()
 						
 						# Close the bracket
 						extra="${extra}\}"
+						
+						# Add the rest of the syntax
+						extra=",extra\=${extra}"
 
 					fi
 					
@@ -325,7 +331,7 @@ function initial_conditions()
 					# interface:
 					# fmoz,fln,mm5camxinfile,outfile_bc,nlevs,nspec,note,xorg,yorg,delx,dely,ibdate,extra
 					# we need to multiply the resolution by 1000 (metre) 
-					echo "$(basename ${CXR_IC_PROC_INPUT_FILE} .pro),'${CXR_MOZART_INPUT_FILE}','${CXR_METEO_INPUT_FILE}','${CXR_ZP_INPUT_FILE}','${CXR_IC_ASC_OUTPUT_FILE}','${CXR_TOPCONC_OUTPUT_FILE}',$NLEV,$MOZART_ARRAY,$CAMX_ARRAY,'${CXR_RUN}',$CXR_MASTER_ORIGIN_XCOORD,$CXR_MASTER_ORIGIN_YCOORD,$(cxr_common_fp_calculate "$CXR_MASTER_CELL_XSIZE * 1000"),$(cxr_common_fp_calculate "$CXR_MASTER_CELL_YSIZE * 1000"),'$IBDATE',extra=$extra" >> ${EXEC_TMP_FILE}
+					echo "$(basename ${CXR_IC_PROC_INPUT_FILE} .pro),'${CXR_MOZART_INPUT_FILE}','${CXR_METEO_INPUT_FILE}','${CXR_ZP_INPUT_FILE}','${CXR_IC_ASC_OUTPUT_FILE}','${CXR_TOPCONC_OUTPUT_FILE}',$NLEV,$MOZART_ARRAY,$CAMX_ARRAY,'${CXR_RUN}',$CXR_MASTER_ORIGIN_XCOORD,$CXR_MASTER_ORIGIN_YCOORD,$(cxr_common_fp_calculate "$CXR_MASTER_CELL_XSIZE * 1000"),$(cxr_common_fp_calculate "$CXR_MASTER_CELL_YSIZE * 1000"),'$IBDATE'$extra" >> ${EXEC_TMP_FILE}
 					echo "exit" >> ${EXEC_TMP_FILE}
 					
 					# Get a copy of the call
