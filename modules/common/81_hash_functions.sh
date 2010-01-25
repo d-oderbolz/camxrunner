5ff554ca5a4dfa2c964573f74920d9777129d33e
Daniel C. Oderbolz <daniel.oderbolz@psi.ch>
2010-01-25 14:03:12 +0000

    %B

--- modules/common/81_hash_functions.sh
+++ modules/common/81_hash_functions.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 #
 # Common script for the CAMxRunner 
-# See http://people.web.psi.ch/oderbolz/CAMxRunner 
+# See http://people.web.psi.ch/oderbolz/CAMxRunner
 #
-# Version: $Id: 80_array_functions.sh 2414 2010-01-23 21:39:07Z oderbolz $ 
+# Version: $Id$ 
 #
 # Contains the Hash Functions, allows to map stirngs (keys) to other strings (values).
 #
@@ -46,7 +46,7 @@ CXR_META_MODULE_AUTHOR="Daniel C. Oderbolz (2008 - 2009), CAMxRunner@psi.ch"
 CXR_META_MODULE_LICENSE="Creative Commons Attribution-Share Alike 2.5 Switzerland (http://creativecommons.org/licenses/by-sa/2.5/ch/deed.en)"
 
 # Do not change this line, but make sure to run "svn propset svn:keywords "Id" FILENAME" on the current file
-CXR_META_MODULE_VERSION='$Id: 80_array_functions.sh 2414 2010-01-23 21:39:07Z oderbolz $'
+CXR_META_MODULE_VERSION='$Id$'
 
 # just needed for stand-alone usage help
 progname=$(basename $0)
