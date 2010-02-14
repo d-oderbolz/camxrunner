5c0a8b1f7e605484710969593dff2f8dd5de9853
Daniel C. Oderbolz <daniel.oderbolz@psi.ch>
2010-02-14 15:50:26 +0000

    %B

--- modules/preproc/CAMx/4.42/once/40_initial_conditions.sh
+++ modules/preproc/CAMx/4.42/once/40_initial_conditions.sh
@@ -46,7 +46,7 @@ CXR_META_MODULE_TYPE="${CXR_TYPE_PREPROCESS_ONCE}"
 CXR_META_MODULE_NUM_TESTS=0
 
 # This is the run name that is used to test this module
-CXR_META_MODULE_TEST_RUN=base
+CXR_META_MODULE_TEST_RUN=CAMx-v4.42-test-ic-bc-tc
 
 # Min CAMxRunner Version needed (Revision number)
 CXR_META_MODULE_REQ_RUNNER_VERSION=94
@@ -456,7 +456,7 @@ function test_module()
 	TEST_COUNT=1
 	
 	# This is our test run for this module
-	CXR_RUN=CAMx-v4.42-test-ic-bc-tc
+	CXR_RUN=$CXR_META_MODULE_TEST_RUN
 	
 	# Safety measure if script is not called from .
 	MY_DIR=$(dirname $0) && cd $MY_DIR
