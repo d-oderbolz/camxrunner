23b31a90a4bbf243d9f5241e6f5084325e38ddcc
Daniel C. Oderbolz <daniel.oderbolz@psi.ch>
2010-03-18 15:19:50 +0000

    %B

--- modules/preproc/CAMx/4.42/daily/02_create_emissions.sh
+++ modules/preproc/CAMx/4.42/daily/02_create_emissions.sh
@@ -186,12 +186,12 @@ function create_emissions()
 			# TODO: This is global, but must be cleaned after run!!
 			common.hash.init create_emission_weekdays $CXR_HASH_TYPE_GLOBAL
 			
-			if [[ "$(common.hash.has? create_emission_weekdays $CXR_HASH_TYPE_GLOBAL $CXR_WOY)" == true ]] 
+			if [[ "$(common.hash.has? create_emission_weekdays $CXR_HASH_TYPE_GLOBAL $CXR_DOW)" == true ]] 
 			then
 				main.log -v "Weekday $CXR_WOY was already calculated. We will only re-calculate biogenic emissions"
 				bio_only=2
 			else
-				common.hash.add create_emission_weekdays $CXR_HASH_TYPE_GLOBAL $CXR_WOY true
+				common.hash.add create_emission_weekdays $CXR_HASH_TYPE_GLOBAL $CXR_DOW true
 				main.log -v "Weekday $CXR_WOY not yet calculated. We will calculate biogenic and anthropogenic emissions"
 				bio_only=0
 			fi
