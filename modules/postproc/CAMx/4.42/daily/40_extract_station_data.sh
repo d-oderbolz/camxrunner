d0edfb96aa02e8a2eb8160981d1982f101475adf
Daniel C. Oderbolz <daniel.oderbolz@psi.ch>
2010-05-28 09:20:43 +0000

    %B

--- modules/postproc/CAMx/4.42/daily/40_extract_station_data.sh
+++ modules/postproc/CAMx/4.42/daily/40_extract_station_data.sh
@@ -308,7 +308,7 @@ function extract_station_data
 				# as well as a flag to indicate if we look at the master domain or not (for coordinate transformation)
 				# We set this fag to 0 because currently we only run on the innermost domain
 				echo ".run $(basename ${CXR_STATION_PROC_INPUT_FILE})" >> ${exec_tmp_file}
-				echo "$(basename ${CXR_STATION_PROC_INPUT_FILE} .pro),'${CXR_STATION_INPUT_FILE}','${CXR_STATION_OUTPUT_DIR}',${write_header},${CXR_DAY},${CXR_MONTH},${CXR_YEAR},${species_array},${xdim},${ydim},${zdim},${stations_array},'${CXR_METEO_INPUT_FILE}',0" >> ${exec_tmp_file}
+				echo "$(basename ${CXR_STATION_PROC_INPUT_FILE} .pro),'${CXR_STATION_INPUT_FILE}','${CXR_STATION_OUTPUT_DIR}',${write_header},${CXR_DAY},${CXR_MONTH},${CXR_YEAR},${species_array},${xdim},${ydim},${zdim},${stations_array},'${CXR_METEO_INPUT_FILE}','${CXR_MET_MODEL}',0" >> ${exec_tmp_file}
 				echo "exit" >> ${exec_tmp_file}
 				;;
 			extract_nabel_stations)
