138019c5702ed1b2ca3caca50926ab22031b5045
Daniel C. Oderbolz <daniel.oderbolz@psi.ch>
2010-06-18 08:00:06 +0000

    %B

--- modules/model/PMCAMx/2008/10_model.sh
+++ modules/model/PMCAMx/2008/10_model.sh
@@ -630,7 +630,7 @@ function execute_model()
 	
 	if [[ $retval -ne 0 ]]
 	then
-		main.log -e "CAMx has returned a non-zero status for $CXR_DATE"
+		main.log -w "PMCAMx has returned a non-zero status for $CXR_DATE"
 	fi
 	
 	# go back
