02f19ee75cb63bb420702ce28b786f8aeac2db95
Daniel C. Oderbolz <daniel.oderbolz@psi.ch>
2010-08-02 20:09:52 +0000

    %B

--- modules/common/state_functions.sh
+++ modules/common/state_functions.sh
@@ -283,6 +283,9 @@ function common.state.updateInfo()
 						elif [[ $field == CXR_META_MODULE_RUN_EXCLUSIVELY ]]
 						then
 							${CXR_SQLITE_EXEC} "$CXR_STATE_DB_FILE" "UPDATE modules SET exclusive=trim('$value') WHERE module='$module'"
+						else
+							# Nothing special
+							${CXR_SQLITE_EXEC} "$CXR_STATE_DB_FILE" "INSERT INTO metadata (module,field,value) VALUES ('$module','$field','$value')"
 						fi # is it a special meta field
 					done
 					
