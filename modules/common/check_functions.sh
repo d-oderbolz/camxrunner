46d5c677ca728cb8183c4a95e4cc6b831a932d47
Daniel C. Oderbolz <daniel.oderbolz@psi.ch>
2010-11-24 09:28:23 +0000

    %B

--- modules/common/check_functions.sh
+++ modules/common/check_functions.sh
@@ -831,7 +831,7 @@ function common.check.preconditions()
 			fi
 			
 			# Writing to link allowed?
-			if [[ "${CXR_ALLOW_WRITING_TO_LINKS}" == false && "$(common.fs.isLink? $(dirname "${output_file}")) == true ]]
+			if [[ "${CXR_ALLOW_WRITING_TO_LINKS}" == false && "$(common.fs.isLink? $(dirname "${output_file}"))" == true ]]
 			then
 				main.dieGracefully "You are trying to write to $_target but CXR_ALLOW_WRITING_TO_LINKS is false!"
 			fi
