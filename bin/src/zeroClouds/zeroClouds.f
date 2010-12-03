1d5a8fc57526e49c44b7758cbd3576385f420815
Daniel C. Oderbolz <daniel.oderbolz@psi.ch>
2010-12-03 07:37:18 +0000

    %B

--- bin/src/zeroClouds/zeroClouds.f
+++ bin/src/zeroClouds/zeroClouds.f
@@ -33,7 +33,7 @@ c
       integer kup,kdn,nlu
       real zm(mnzc),zz(mnzc),thetav(mnzc),uwind(mnzc),vwind(mnzc),
      &     tkep(mnzc),rkc(mnzc),tt(mnzc),pp(mnzc),qv(mnzc),cc(mnzc)
-      real lwat,zero,tkemin,gamma,dxcamx,dycamx,x0camx,y0camx,
+      real lwat,zero,dxcamx,dycamx,x0camx,y0camx,
      &     deltax,hr,hrp,th,qq,press0,wind,ustar,eli,wstar,
      &     sumtkz,sumtk,xinf,q,z,dz,eee,al1,rwat,swat,gwat,delz,volrat,
      &     clonin,clatin,tlat1in,tlat2in,sumf,sumff
@@ -47,8 +47,6 @@ c
       data ltopo /.true./
       data lfill /.true./
       data cldhdr /'CAMx_V4.3 CLOUD_RAIN'/
-      data tkemin /1.e-6/
-      data gamma /0.286/
 c
 c-----Initialize variables
 c
@@ -59,7 +57,6 @@ c
 c
 c-----Get user-supplied inputs
 c
-
 
       read(*,'(20x,a)') fname
       read(fname,*) sdate,edate
@@ -79,11 +76,11 @@ c
         edate = addday(edate)
       endif
       
-c      stime = 100*stime
-c      etime = 100*etime
+      stime = 100*stime
+      etime = 100*etime
       
-c     write(*,'(a,i6,i6)') 'Start date/time (YYJJJ HHMM):',sdate,stime
-c      write(*,'(a,i6,i6)') 'End date/time (YYJJJ HHMM):',edate,etime
+     write(*,'(a,i6,i6)') 'Start date/time (YYJJJ HHMM):',sdate,stime
+     write(*,'(a,i6,i6)') 'End date/time (YYJJJ HHMM):',edate,etime
 
 c    This command is the reason why start and end must be in the same year
      ndays = edate - sdate
@@ -93,8 +90,7 @@ c    This command is the reason why start and end must be in the same year
         stop
      endif
      
-     write(*,'(a,i)') '     Number of days:',
-     &                          ndays
+     write(*,'(a,i)') '     Number of days:',ndays
       
       read(*,'(20x,a)') fname
       read(fname,*) dtout
@@ -124,120 +120,120 @@ c    This command is the reason why start and end must be in the same year
 c
 c-----Special date/time variables for precip and clouds
 c
-        jdatep = jdate
-        jhrp = jhr/100
-        jmnp = mod(jhr,100)
-        if (dtout.lt.60) then
-          jmnp = jmnp - dtout
-          if (jmnp.lt.0) then
-            jmnp = jmnp + 60
-            jhrp = jhrp - 1
-            if (jhrp.lt.0) then
-              jhrp = jhrp + 24
-              jdatep = subday(jdatep)
-            endif
-          endif
-        else
-          jhrp = jhrp - dtout/60
-          if (jhrp.lt.0) then
-            jhrp = jhrp + 24
-            jdatep = subday(jdatep)
-          endif
-        endif
-        jhrp = 100*jhrp + jmnp
-        hrp = float(jhrp)
-
-        do j = 1,nyc
-          do i = 1,nxc
-
-c
-c-----Prepare cloud fields
-c
-					
-            do k = 1,nzc
-              cwc(i,j,k) = 0.
-              pwr(i,j,k) = 0.
-              pws(i,j,k) = 0.
-              pwg(i,j,k) = 0.
-              cod(i,j,k) = 0.
-            enddo
-
-          enddo
-        enddo
-c
-c-----Write to CAMx files
-c
-
-c
-c-----For each day
-c
-
-        do currdate = sdate, edate
-
-c
-c-----Time-varying met fields
-c
-
-        do hr = 0,23
-
-        junit = 15
-        
-        hrp = float(100 * hr)
-        
-        write(*,'(a,i6.5,i5.4,/)')'Cld/rn date/time (YYJJJ HHMM):',&
-        currdate,hr
-     
-        write(junit) hrp,currdate
-        do k = 1,nzc
-          write(junit) ((cwc(i,j,k),i=1,nxc),j=1,nyc) 
-          write(junit) ((pwr(i,j,k),i=1,nxc),j=1,nyc) 
-          write(junit) ((pws(i,j,k),i=1,nxc),j=1,nyc) 
-          write(junit) ((pwg(i,j,k),i=1,nxc),j=1,nyc) 
-          write(junit) ((cod(i,j,k),i=1,nxc),j=1,nyc) 
-        enddo 
-c---- End hours
-      enddo
-
-c---- End days
-      enddo
-
-      write(*,'(/,a,/)')' program run complete'
-
-      stop
-      end
-c
-c-----Date functions
-c
-      integer function addday(idate)
-      implicit none
-      integer idate,iyr,idy
-      iyr = idate/1000
-      idy = idate - iyr*1000
-      if ((mod(iyr,4).eq.0 .and. idy.eq.366) .or.
-     &    (mod(iyr,4).ne.0 .and. idy.eq.365)) then
-        iyr = iyr + 1
-        if (iyr.gt.99) iyr = 0
-        addday = iyr*1000 + 1
-      else
-        addday = idate + 1
-      endif
-      end
-c
-      integer function subday(idate)
-      implicit none
-      integer idate,iyr,idy
-      iyr = idate/1000
-      idy = idate - iyr*1000
-      if (idy.eq.1) then
-        iyr = iyr - 1
-        if (iyr.lt.0) iyr = 99
-        if (mod(iyr,4).eq.0) then
-          idy = 366
-        else
-          idy = 365
-        endif
-        subday = iyr*1000 + idy
-      else
-        subday = idate - 1
-      endif
+!        jdatep = jdate
+!        jhrp = jhr/100
+!        jmnp = mod(jhr,100)
+!        if (dtout.lt.60) then
+!          jmnp = jmnp - dtout
+!          if (jmnp.lt.0) then
+!            jmnp = jmnp + 60
+!            jhrp = jhrp - 1
+!            if (jhrp.lt.0) then
+!              jhrp = jhrp + 24
+!              jdatep = subday(jdatep)
+!            endif
+!          endif
+!        else
+!          jhrp = jhrp - dtout/60
+!          if (jhrp.lt.0) then
+!            jhrp = jhrp + 24
+!            jdatep = subday(jdatep)
+!          endif
+!        endif
+!        jhrp = 100*jhrp + jmnp
+!        hrp = float(jhrp)
+!
+!        do j = 1,nyc
+!          do i = 1,nxc
+!
+!c
+!c-----Prepare cloud fields
+!c
+!					
+!            do k = 1,nzc
+!              cwc(i,j,k) = 0.
+!              pwr(i,j,k) = 0.
+!              pws(i,j,k) = 0.
+!              pwg(i,j,k) = 0.
+!              cod(i,j,k) = 0.
+!            enddo
+!
+!          enddo
+!        enddo
+!c
+!c-----Write to CAMx files
+!c
+!
+!c
+!c-----For each day
+!c
+!
+!        do currdate = sdate, edate
+!
+!c
+!c-----Time-varying met fields
+!c
+!
+!        do hr = 0,23
+!
+!        junit = 15
+!        
+!        hrp = float(100 * hr)
+!        
+!        write(*,'(a,i6.5,i5.4,/)')'Cld/rn date/time (YYJJJ HHMM):',&
+!        currdate,hr
+!     
+!        write(junit) hrp,currdate
+!        do k = 1,nzc
+!          write(junit) ((cwc(i,j,k),i=1,nxc),j=1,nyc) 
+!          write(junit) ((pwr(i,j,k),i=1,nxc),j=1,nyc) 
+!          write(junit) ((pws(i,j,k),i=1,nxc),j=1,nyc) 
+!          write(junit) ((pwg(i,j,k),i=1,nxc),j=1,nyc) 
+!          write(junit) ((cod(i,j,k),i=1,nxc),j=1,nyc) 
+!        enddo 
+!c---- End hours
+!      enddo
+!
+!c---- End days
+!      enddo
+!
+!      write(*,'(/,a,/)')' program run complete'
+!
+!      stop
+!      end
+!c
+!c-----Date functions
+!c
+!      integer function addday(idate)
+!      implicit none
+!      integer idate,iyr,idy
+!      iyr = idate/1000
+!      idy = idate - iyr*1000
+!      if ((mod(iyr,4).eq.0 .and. idy.eq.366) .or.
+!     &    (mod(iyr,4).ne.0 .and. idy.eq.365)) then
+!        iyr = iyr + 1
+!        if (iyr.gt.99) iyr = 0
+!        addday = iyr*1000 + 1
+!      else
+!        addday = idate + 1
+!      endif
+!      end
+!c
+!      integer function subday(idate)
+!      implicit none
+!      integer idate,iyr,idy
+!      iyr = idate/1000
+!      idy = idate - iyr*1000
+!      if (idy.eq.1) then
+!        iyr = iyr - 1
+!        if (iyr.lt.0) iyr = 99
+!        if (mod(iyr,4).eq.0) then
+!          idy = 366
+!        else
+!          idy = 365
+!        endif
+!        subday = iyr*1000 + idy
+!      else
+!        subday = idate - 1
+!      endif
       end
