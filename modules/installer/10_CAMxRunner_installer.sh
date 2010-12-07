9ec7bf1963f8281c643628957a7b2698353c2cdf
Daniel C. Oderbolz <daniel.oderbolz@psi.ch>
2010-12-07 11:02:34 +0000

    %B

--- modules/installer/10_CAMxRunner_installer.sh
+++ modules/installer/10_CAMxRunner_installer.sh
@@ -218,7 +218,7 @@ function CAMxRunner_installer()
 					export CPPFLAGS="-I${CXR_LIB_DIR}/lzo/${HOSTTYPE}/lzo"
 					export CFLAGS="$CPPFLAGS"
 					export LDFLAGS="-L${CXR_LIB_DIR}/lzo/${HOSTTYPE}"
-					export LIBS="-llzop2"
+					export LIBS="-llzo2"
 					export LD_LIBRARY_PATH="${CXR_LIB_DIR}/lzo/${HOSTTYPE}:${LD_LIBRARY_PATH}"
 					
 					./configure --prefix=${CXR_BIN_DIR} \
