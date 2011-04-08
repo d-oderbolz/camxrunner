dfc30eba49e23e31b08d45103947866085445a2c
Daniel C. Oderbolz <daniel.oderbolz@psi.ch>
2011-04-08 10:08:00 +0000

    %B

--- bin/CAMx/4.42/src/camxpost/camxpost.f
+++ bin/CAMx/4.42/src/camxpost/camxpost.f
@@ -23,7 +23,7 @@ c      output will not be performed
 c     -If the gridded output file names are not supplied, they will not be
 c      generated
 c
-      parameter (mxx=200,mxy=200,mxspc=30,mxhr=48,mxsit=100,mxobs=200)
+      parameter (mxx=300,mxy=300,mxspc=150,mxhr=48,mxsit=100,mxobs=200)
       integer ibgdat(mxhr),ndate(mxobs),nhour(mxobs),
      &        nuse(mxhr),nmax(mxsit),iostatus
       real cread(mxx,mxy),xutm(mxsit),yutm(mxsit),
